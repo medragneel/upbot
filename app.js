a1c958f06d09c3447597c8f5c3a8ea0da4c25f45
mshadow <mshadow@pop-os.localdomain>
2023-09-29 17:31:29 +0100

    changed to async await

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-require("dotenv").config()
 const TelegramBot = require('node-telegram-bot-api');
 const axios = require('axios');
+require("dotenv").config()
 
-const bot = new TelegramBot(process.env.TM_TOKEN, { polling: true, pollingInterval: 1000 });
+const bot = new TelegramBot(process.env.TM_TOKEN, {
+    polling: {
+        interval:500,
+        autoStart:true,
+        params:{
+            timeout: 10 ,
+        }
+    }
+});
 
-
-bot.on('message', (message) => {
+bot.on('message', async (message) => {
     if (message.text) {
         const searchQuery = message.text.trim();
-        axios.get(`https://api.themoviedb.org/3/search/multi?api_key=${process.env.API_KEY}&query=${searchQuery}`)
+        await axios.get(`https://api.themoviedb.org/3/search/multi?api_key=${process.env.API_KEY}&query=${searchQuery}`)
             .then(response => {
                 const results = response.data.results;
                 if (results.length > 0) {
@@ -20,51 +27,30 @@ bot.on('message', (message) => {
                         Type: ${res.media_type}\n
                         https://image.tmdb.org/t/p/w500/${res.poster_path}\n
                         `
-
-                        if (res.media_type === "movie") {
+                        if (res.media_type === "movie" || res.media_type === "tv") {
                             const links = `
                             Links: \n
                             vidsrc:\n
-                            https://vidsrc.to/embed/movie/${res.id}\n
+                            https://vidsrc.to/embed/${res.media_type}/${res.id}\n
                             SuperEmbed:\n
-                            https://multiembed.mov/?video_id=${res.id}&tmdb=1 \n
+                            https://multiembed.com/?video_id=${res.id}&tmdb=1 \n
                             seez.su \n
-                            https://seez.su/movie/${res.id} \n
+                            https://seez.su/${res.media_type}/${res.id} \n
                             movie-web: \n
-                            https://movie-web.app/search/movie/${searchQuery.replace(" ", "%20")} \n
-                            smashystream: \n
-                            https://embed.smashystream.com/playere.php?tmdb=${res.id} \n
+                            https://movie-web.app/search/${res.media_type}/${searchQuery.replace(" ", "%20")}
                             `
-                            bot.sendMessage(message.chat.id, msg + links)
-
-                        } else if (res.media_type === "tv") {
-                            const links = `
-                            vidsrc:\n
-                            https://vidsrc.to/embed/tv/${res.id} \n
-                            SuperEmbed:\n
-                            https://multiembed.mov/?video_id=${res.id}&tmdb=1&s=1&e=1 \n
-                            seez.su \n
-                            https://seez.su/tv/${res.id} \n
-                            movie-web: \n
-                            https://movie-web.app/search/series/${searchQuery.replace(" ", "%20")}
-                            smashystream: \n
-                             https://embed.smashystream.com/playere.php?tmdb=${res.id}&season=1&episode=1 \n
-                            `
-                            bot.sendMessage(message.chat.id, msg + links)
-
-
+                            return bot.sendMessage(message.chat.id, msg + links);
                         } else {
-                            bot.sendMessage(message.chat.id, 'not found movie or tv show')
-
+                            return bot.sendMessage(message.chat.id, 'Not found movie or tv show');
                         }
                     })
-
                 } else {
-                    bot.sendMessage(message.chat.id, 'No movies found');
+                    return bot.sendMessage(message.chat.id, 'No movies found');
                 }
             })
             .catch(error => {
                 console.error(error);
+                return bot.sendMessage(message.chat.id, 'Error searching for movie');
             });
     }
 });
